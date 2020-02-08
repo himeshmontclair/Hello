c52256d7cdffc429d473e0ffed00e2a7ebdb2cf4
himeshmontclair <dameguntah1@montclair.edu>
2020-02-07 22:04:07 -0500

    Hello github

---
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hello;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.buttonClick);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Word!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+}
+}
